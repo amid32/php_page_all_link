38fc4035a1f2d7b89c13e4045b420122085b781a
Amid Mirzeyev <amidmirzeyev32@gmail.com>
2019-07-27 18:05:20 +0400

    test

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php 
+$demo = "CONTACT";
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?=$demo; ?></title>
+</head>
+<body>
+	<?php include 'link/link.php'; ?>
+<h1>Welcome: <?=$demo; ?></h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod <br>
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <br>
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo <br>
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse <br>
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non <br>
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</body>
+</html>
---
+++ link/link.php
@@ -0,0 +1,6 @@
+<nav>
+		<a href="index.php">HOME</a>
+		<a href="contact.php">CONTACT</a>
+		<a href="about.php">ABOUT</a>
+
+	</nav>
